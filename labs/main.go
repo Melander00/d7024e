f59a91510b6273e4ff46016900b86a1ea93dbf38
Samuel <cool23man23@gmail.com>
2026-09-09 07:58:21 +0200

    Created dockerfile and tried with fixed main.go

--- labs/main.go
+++ labs/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	fmt.Println("Pretending to run the kademlia app...")
 	// Using stuff from the kademlia package here. Something like...
-	id := kademlia.NewKademliaID("FFFFFFFF00000000000000000000000000000000")
+	id := kademlia.NewKademliaID("FFFFFFFF00000000000000000000000000000000000000000000000000000000")
 	contact := kademlia.NewContact(id, "localhost:8000")
 	fmt.Println(contact.String())
 	fmt.Printf("%v\n", contact)

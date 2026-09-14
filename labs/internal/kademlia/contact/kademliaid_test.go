6d6225112ca31e840a19668e4fa2426685b3249f
Samuel <cool23man23@gmail.com>
2026-09-14 13:25:40 +0200

    id and routing table > 95% coverage

--- labs/internal/kademlia/contact/kademliaid_test.go
+++ labs/internal/kademlia/contact/kademliaid_test.go
@@ -1,6 +1,106 @@
 package contact
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
+
+func TestParseKademliaID(t *T) {
+	hex := "FFFFFFFF00000000000000000000000000000000000000000000000000000000"
+
+	id, _ := ParseKademliaID(hex)
+
+	if strings.ToLower(id.String()) != strings.ToLower(hex) {
+		t.Fatalf("invalid ID %s should've been %s", id.String(), hex)
+	}
+}
+
+func TestParseInvalidKademliaID(t *T) {
+	// Too short
+	_, err := ParseKademliaID("FFFFFFFF000000000000000000000000000000000000000000000000000000")
+	if err == nil {
+		t.Fatalf("parser didnt catch too short")
+	}
+	// Too long
+	_, err = ParseKademliaID("FFFFFFFF0000000000000000000000000000000000000000000000000000000000")
+	if err == nil {
+		t.Fatalf("parser didnt catch too long")
+	}
+	// Invalid hex
+	_, err = ParseKademliaID("ZZFFFFFF00000000000000000000000000000000000000000000000000000000")
+	if err == nil {
+		t.Fatalf("parser didnt catch invalid hex")
+	}
+}
+
+func TestNewIDFromAddress(t *T) {
+	addr := "127.0.0.1:10001"
+
+	id, _ := NewKademliaIDFromAddress(addr)
+
+	// Sha256 generated from outside golang
+	realId, _ := ParseKademliaID("02b5b94b75a96720af96f713795941897f69cad9acafa3b2f8380089c7ee3c07")
+
+	if !id.Equals(realId) {
+		t.Fatalf("hash is wrong %s but should've been %s", id.String(), realId.String())
+	}
+}
+
+func TestNewIDFromInvalidAddress(t *T) {
+	addr := "some-faulty-address"
+
+	_, err := NewKademliaIDFromAddress(addr)
+
+	if err == nil {
+		t.Fatalf("didnt catch invalid address")
+	}
+}
+
+func TestIDLess(t *T) {
+	id1, _ := ParseKademliaID("FFFFFFFF00000000000000000000000000000000000000000000000000000000")
+	id2, _ := ParseKademliaID("1111111100000000000000000000000000000000000000000000000000000000")
+
+	if !id2.Less(id1) {
+		t.Fatalf("%s was less than %s", id1.String(), id2.String())
+	}
+}
+
+func TestIDEquals(t *T) {
+
+	hex := "FFFFFFFF00000000000000000000000000000000000000000000000000000000"
+
+	id1, _ := ParseKademliaID(hex)
+	id2, _ := ParseKademliaID(hex)
+
+	if !id1.Equals(id2) {
+		t.Fatalf("invalid identical ids %s | %s", id1.String(), id2.String())
+	}
+}
+
+func TestIDDistance(t *T) {
+	id1, _ := ParseKademliaID("FFFFFFFF00000000000000000000000000000000000000000000000000000000")
+	id2, _ := ParseKademliaID("1111111100000000000000000000000000000000000000000000000000000000")
+
+	d, _ := ParseKademliaID("EEEEEEEE00000000000000000000000000000000000000000000000000000000")
+
+	if !id1.CalcDistance(id2).Equals(d) {
+		t.Fatalf("distance calculation is wrong, got %s but should be %s", id1.CalcDistance(id2), d)
+	}
+
+	ab := id1.CalcDistance(id2)
+	ba := id2.CalcDistance(id1)
+
+	if !ab.Equals(ba) {
+		t.Fatalf("a XOR b should equal b XOR a, %s | %s", ab, ba)
+	}
+
+	zeroD, _ := ParseKademliaID("0000000000000000000000000000000000000000000000000000000000000000")
+
+	if !id1.CalcDistance(id1).Equals(zeroD) {
+		t.Fatalf("a XOR a should be zero")
+	}
+
+}
 
 func TestNewRandomKademliaID(t *testing.T) {
 	id := NewRandomKademliaID()
--- labs/internal/kademlia/contact/routingtable_test.go
+++ labs/internal/kademlia/contact/routingtable_test.go
@@ -1,33 +1,142 @@
 package contact
 
 import (
-	"fmt"
 	"testing"
 )
+
+type T = testing.T
+
+func TestNewRoutingTable(t *T) {
+	me := NewContact(
+		parseTestKademliaID(t, "FFFFFFFF00000000000000000000000000000000000000000000000000000000"),
+		"localhost:8000",
+	)
+
+	k := 10
+
+	rt := NewRoutingTable(me, k)
+
+	if rt == nil {
+		t.Fatal("routing table nil")
+	}
+
+	for i, bucket := range rt.buckets {
+		if bucket == nil {
+			t.Fatalf("bucket %d is nil", i)
+		}
+		if bucket.size != k {
+			t.Fatalf("bucket %d has size %d instead of %d", i, bucket.size, k)
+		}
+	}
+}
+
+func TestRoutingTableFindClosestContacts(t *T) {
+	me := NewContact(
+		parseTestKademliaID(t, "FFFFFFFF00000000000000000000000000000000000000000000000000000000"),
+		"localhost:8000",
+	)
+
+	rt := NewRoutingTable(me, 10)
+
+	ids := []*KademliaID{
+		parseTestKademliaID(t, "1111111100000000000000000000000000000000000000000000000000000000"),
+		parseTestKademliaID(t, "1111111200000000000000000000000000000000000000000000000000000000"),
+		parseTestKademliaID(t, "1111111300000000000000000000000000000000000000000000000000000000"),
+		parseTestKademliaID(t, "1111111400000000000000000000000000000000000000000000000000000000"),
+		parseTestKademliaID(t, "2111111400000000000000000000000000000000000000000000000000000000"),
+	}
+
+	contacts := []Contact{
+		NewContact(
+			ids[0],
+			"localhost:8001",
+		),
+		NewContact(
+			ids[1],
+			"localhost:8002",
+		),
+		NewContact(
+			ids[2],
+			"localhost:8003",
+		),
+		NewContact(
+			ids[3],
+			"localhost:8004",
+		),
+		NewContact(
+			ids[4],
+			"localhost:8005",
+		),
+	}
+
+	for _, contact := range contacts {
+		rt.AddContact(contact)
+	}
+
+	target := ids[4]
+
+	closest := rt.FindClosestContacts(target, 3)
+
+	if len(closest) != 3 {
+		t.Fatalf("expected 3 contacts, got %d", len(closest))
+	}
+
+	// The target itself is included because it is a normal contact.
+	expected := []string{
+		"2111111400000000000000000000000000000000000000000000000000000000",
+		"1111111400000000000000000000000000000000000000000000000000000000",
+		"1111111100000000000000000000000000000000000000000000000000000000",
+	}
+
+	for i, contact := range closest {
+		if !contact.ID.Equals(parseTestKademliaID(t, expected[i])) {
+			t.Fatalf(
+				"position %d: expected %s, got %s",
+				i,
+				expected[i],
+				contact.ID,
+			)
+		}
+	}
+}
+
+func TestRoutingTableDoesNotAddSelf(t *T) {
+	me := NewContact(
+		parseTestKademliaID(t, "FFFFFFFF00000000000000000000000000000000000000000000000000000000"),
+		"localhost:8000",
+	)
+
+	rt := NewRoutingTable(me, 10)
+
+	rt.AddContact(me)
+
+	contacts := rt.FindClosestContacts(me.ID, 20)
+
+	if len(contacts) != 0 {
+		t.Fatalf("expected 0 contacts, got %d", len(contacts))
+	}
+}
 
 // FIXME: This test doesn't actually test anything. There is only one assertion
 // that is included as an example.
 
-func TestRoutingTable(t *testing.T) {
-	rt := NewRoutingTable(NewContact(parseTestKademliaID(t, "FFFFFFFF00000000000000000000000000000000000000000000000000000000"), "localhost:8000"), 10)
+// func TestRoutingTable(t *testing.T) {
+// 	rt := NewRoutingTable(NewContact(parseTestKademliaID(t, "FFFFFFFF00000000000000000000000000000000000000000000000000000000"), "localhost:8000"), 10)
 
-	rt.AddContact(NewContact(parseTestKademliaID(t, "FFFFFFFF00000000000000000000000000000000000000000000000000000000"), "localhost:8001"))
-	rt.AddContact(NewContact(parseTestKademliaID(t, "1111111100000000000000000000000000000000000000000000000000000000"), "localhost:8002"))
-	rt.AddContact(NewContact(parseTestKademliaID(t, "1111111200000000000000000000000000000000000000000000000000000000"), "localhost:8002"))
-	rt.AddContact(NewContact(parseTestKademliaID(t, "1111111300000000000000000000000000000000000000000000000000000000"), "localhost:8002"))
-	rt.AddContact(NewContact(parseTestKademliaID(t, "1111111400000000000000000000000000000000000000000000000000000000"), "localhost:8002"))
-	rt.AddContact(NewContact(parseTestKademliaID(t, "2111111400000000000000000000000000000000000000000000000000000000"), "localhost:8002"))
+// 	rt.AddContact(NewContact(parseTestKademliaID(t, "FFFFFFFF00000000000000000000000000000000000000000000000000000000"), "localhost:8001"))
+// 	rt.AddContact(NewContact(parseTestKademliaID(t, "1111111100000000000000000000000000000000000000000000000000000000"), "localhost:8002"))
+// 	rt.AddContact(NewContact(parseTestKademliaID(t, "1111111200000000000000000000000000000000000000000000000000000000"), "localhost:8002"))
+// 	rt.AddContact(NewContact(parseTestKademliaID(t, "1111111300000000000000000000000000000000000000000000000000000000"), "localhost:8002"))
+// 	rt.AddContact(NewContact(parseTestKademliaID(t, "1111111400000000000000000000000000000000000000000000000000000000"), "localhost:8002"))
+// 	rt.AddContact(NewContact(parseTestKademliaID(t, "2111111400000000000000000000000000000000000000000000000000000000"), "localhost:8002"))
+// 	rt.AddContact(NewContact(parseTestKademliaID(t, "FFFFFFFFFF000000000000000000000000000000000000000000000000000000"), "localhost:8002"))
 
-	contacts := rt.FindClosestContacts(parseTestKademliaID(t, "2111111400000000000000000000000000000000000000000000000000000000"), 20)
-	for i := range contacts {
-		fmt.Println(contacts[i].String())
-	}
-
-	// TODO: This is just an example. Make more meaningful assertions.
-	if len(contacts) != 6 {
-		t.Fatalf("Expected 6 contacts but instead got %d", len(contacts))
-	}
-}
+// 	// Test dont add self to routing table
+// 	contacts := rt.FindClosestContacts(parseTestKademliaID(t, "2111111400000000000000000000000000000000000000000000000000000000"), 20)
+// 	if len(contacts) != 5 {
+// 		t.Fatalf("Expected 5 contacts but instead got %d", len(contacts))
+// 	}
+// }
 
 func parseTestKademliaID(t *testing.T, value string) *KademliaID {
 	t.Helper()

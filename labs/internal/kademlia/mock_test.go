adf194291ecf3df6ce3038fdfc0a0b3cc461d737
Samuel <cool23man23@gmail.com>
2026-09-15 08:58:17 +0200

    supress stdout during mock test

--- labs/internal/kademlia/mock_test.go
+++ labs/internal/kademlia/mock_test.go
@@ -1,6 +1,8 @@
 package kademlia
 
 import (
+	"io"
+	"os"
 	"testing"
 )
 
@@ -9,5 +11,18 @@ type T = testing.T
 func TestMock(t *T) {
 	// Is there even any real reason to unit-test mocks?
 	// It is just for demonstration so by adding this line we reach the 80% coverage goal.
+	old := os.Stdout
+
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
 	MockKademlia(100)
+
+	// Restore stdout.
+	w.Close()
+	os.Stdout = old
+
+	// Drain the pipe if necessary.
+	io.Copy(io.Discard, r)
+	r.Close()
 }
